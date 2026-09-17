93e9c215d6a0c5a9c82c4f41b084f9e3c55ea5f3
Aman Antuley <amanantuley3794@gmail.com>
2026-09-17 15:26:42 +0530

    CORS from frontend to backend

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_methods=["*"]
 )
 
